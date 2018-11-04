8787430ff51dd3c2c1f03603f8ea08d4a1114c0e
Zhzer <zhz961101@gmail.com>
2018-11-04 21:41:47 +0800

    npm

--- README.md
+++ README.md
@@ -1,28 +1,60 @@
-# JSON_safe
+# JSON_smart
 JSON tool kit, support circular structure / solve Uncaught TypeError.
+
+# Installation
+```
+$ npm install json-smart
+```
 
 # Example
 ```js
-const JSON_safe = require('JSON_safe');
+const JSON_smart = require('JSON_smart');
 var obj = {
     version: "1.0.1",
+    g: typeof(window) !== "undefined"?window:global,
     circularOBJ: {},
     deepCircular: {}
 }
 obj.circularOBJ.self = obj.circularOBJ;
 obj.deepCircular = {
-    one:{
-        tow:{
-            more:{
+    one: {
+        tow: {
+            more: {
                 root: obj
             }
         }
     }
 }
-var json = JSON_safe.stringify(obj);
-var parsed = JSON_safe.parse(json);
+obj.deepCircular.self = obj.deepCircular
+var json = JSON_smart.stringify(obj);
+var parsed = JSON_smart.parse(json);
 console.log(json);
 console.log(parsed);
 ```
+output:
+```
+{
+	"version": "1.0.1",
+	"g": "[Global Object]",
+	"circularOBJ": {
+		"self": "[CIRCULAR ~.circularOBJ]"
+	},
+	"deepCircular": {
+		"one": {
+			"tow": {
+				"more": {
+					"root": "[CIRCULAR ~]"
+				}
+			}
+		},
+		"self": "[CIRCULAR ~.deepCircular]"
+	}
+}
 
+{version: "1.0.1", g: Window, circularOBJ: {…}, deepCircular: {…}}
+```
 # Usage
+like JSON.
+
+# About
+have fun.
--- json.js
+++ json.js
@@ -3,12 +3,8 @@
  * @author zhzluke96
  */
 
-// Partial function
-const isType = type => obj => (new RegExp(type,"i")).test(Object.prototype.toString.call(obj).replace(/^\[object /, "").replace(/\]$/, ""));
+const is = require('is-obj-type');
 const ref2obj = ref_string => root => ref_string ? eval("root." + ref_string) : root;
-
-// global object not stringify,mark placeholder only
-const isGlobal = obj => isType("Window")(obj) || isType("Global")(obj) || isType("DOMWindow")(obj);
 const global_placeholder = "[Global Object]";
 
 // circular placeholder
@@ -18,14 +14,14 @@ const circular_placeholder = "CIRCULAR";
 const repG = obj => {
     var cache = []
     function recu(_o){
-        if(isType("Object")(_o) || isType("Array")(_o)){
+        if(is.obj(_o) || is.arr(_o)){
             for (let vi in _o) {
                 let v = _o[vi];
                 if(~cache.indexOf(v))continue;
                 else cache.push(v);
                 _o[vi] = recu(v);
             }
-        }else if(isGlobal(_o)){
+        }else if(is.global(_o)){
             return global_placeholder;
         }
         return _o;
@@ -45,30 +41,32 @@ function stringify(value, space, replacer) {
     // *String("") will => undefined
     space = space == undefined ? "\t" : (space == "" ? undefined : space);
     let cache = [],
-        keys = [],
-        isObj = isType("Object"),
-        isNull = isType("Null"),
-        replacer_use = isType("Function")(replacer);
+        keys = [];
     // node environ
     if(typeof global !== "undefined")value = repG(value);
     const ret = JSON.stringify(value, function(key, val) {
         if (cache.length) {
             // pop cache
             var cur = cache.indexOf(this);
-            ~cur ? cache.splice(cur + 1) : cache.push(this);
-            ~cur ? keys.splice(cur, Infinity, key) : keys.push(key);
+            if(~cur) {
+                cache.splice(cur + 1);
+                keys.splice(cur, Infinity, key);
+            }else {
+                cache.push(this);
+                keys.push(key);
+            }
             // skip Global ref
-            if (isGlobal(val)) return global_placeholder;
-            if (isObj(val) && !isNull(val)) {
+            if (is.global(val)) return global_placeholder;
+            if (is.obj(val) && !is.null(val)) {
                 // Provided circulation/loop structure path hints
                 // ~ => root path
                 if (~cache.indexOf(val)) return `[${circular_placeholder} ~${[""].concat(keys.slice(0,cache.indexOf(val))).join(".")}]`;
             }
         } else cache.push(value);
-        return replacer_use ? replacer.call(this, key, val) : val;
+        return is.func(replacer) ? replacer.call(this, key, val) : val;
     }, space);
     // manual GC
-    cache = keys = isObj = isNull = null;
+    delete cache,keys;
     return ret;
 }
 
@@ -79,22 +77,16 @@ function stringify(value, space, replacer) {
  * @return {Object}            json => object
  */
 function parse(json, replacer) {
-    let isObj = isType("Object"),
-        isArr = isType("Array"),
-        isFunc = isType("Function"),
-        isStr = isType("String"),
-        replacer_use = isFunc(replacer),
-        re = new RegExp(`\\[${circular_placeholder} ~(\\.(.+))?\\]`);
+    let re = new RegExp(`\\[${circular_placeholder} ~(\\.(.+))?\\]`);
     var ret = JSON.parse(json, (key, val) => {
-        if (isStr(val) && re.test(val)) return ref2obj(val.match(re)[2] || "");
+        if (is.str(val) && re.test(val)) return ref2obj(val.match(re)[2] || "");
         if (val === global_placeholder) return typeof window !== "undefined"? window : (global !== "undefined"? global : (DOMWindow !== "undefined"? DOMWindow : "[UNKNOWN]"));
-        return replacer_use ? replacer.call(this, key, val) : val;
+        return is.func(replacer) ? replacer.call(this, key, val) : val;
     })
     ret = emit(ret);
     // GC
-    delete re, isObj, isArr, isFunc, isStr;
+    delete re;
     return ret;
-
     // Partial function emit
     function emit(obj) {
         var that = obj;
@@ -103,37 +95,12 @@ function parse(json, replacer) {
         function recu(_o) {
             for (let oi in _o) {
                 let o = _o[oi];
-                if (isFunc(o)) _o[oi] = o(that);
-                else if (isObj(o) || isArr(o)) recu(o);
+                if (is.func(o)) _o[oi] = o(that);
+                else if (is.obj(o) || is.arr(o)) recu(o);
             }
             return _o;
         }
     }
 }
 
-// const JSON_safe = require('JSON_safe');
-const JSON_safe = {
-    stringify: stringify,
-    parse: parse
-}
-var obj = {
-    version: "1.0.1",
-    g: typeof(window) !== "undefined"?window:global,
-    circularOBJ: {},
-    deepCircular: {}
-}
-obj.circularOBJ.self = obj.circularOBJ;
-obj.deepCircular = {
-    one: {
-        tow: {
-            more: {
-                root: obj
-            }
-        }
-    }
-}
-obj.deepCircular.self = obj.deepCircular
-var json = JSON_safe.stringify(obj);
-var parsed = JSON_safe.parse(json);
-console.log(json);
-console.log(parsed);
+module.exports = {stringify,parse};
